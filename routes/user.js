b1acc30747779ca3e7aca822f835874bb5479b0c
jason <jason@aistechnolabs.org>
2024-03-22 16:20:06 +0530

    create api for user auithentication, validation, middlaware

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,9 @@
 module.exports = function (app, model, controller) {
 
+    var middleware = require('../app/middleware/index')(model);
+    var validation = require('../app/validation/index')(model);
+
+    app.post('/userRegister',validation.user.userRegister, controller.user.userRegister);
+    app.post('/userLogin',validation.user.userLogin, controller.user.userLogin);
     
 }
